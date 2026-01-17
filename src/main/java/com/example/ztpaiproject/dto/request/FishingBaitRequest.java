15f35b40f6538fbe32d6135d974c99ee4698e8d7
Norbert Wojtowicz <norbi2790@gmail.com>
2026-01-17 16:12:53 +0100

    feat(api/baits): add NotNull adnotation to FishingBaitType request dto

--- src/main/java/com/example/ztpaiproject/dto/request/FishingBaitRequest.java
+++ src/main/java/com/example/ztpaiproject/dto/request/FishingBaitRequest.java
@@ -2,6 +2,7 @@ package com.example.ztpaiproject.dto.request;
 
 import com.example.ztpaiproject.model.enums.FishingBaitType;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -9,6 +10,7 @@ public class FishingBaitRequest {
     @NotBlank(message = "Nazwa przynęty jest wymagana")
     private String name;
 
-    private FishingBaitType type; // NATURAL, ARTIFICIAL, MIX
+    @NotNull(message = "Typ przynęty jest wymagany (NATURAL, ARTIFICIAL, MIX)")
+    private FishingBaitType type;
     private String description;
 }
